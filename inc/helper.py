8e541566e80ea76e602a18fb862bed1833f1d8ff
bison-- <mike.bison42@googlemail.com>
2024-09-13 10:17:51 +0200

    improve

--- inc/helper.py
+++ inc/helper.py
@@ -11,8 +11,10 @@ def git_repo_to_page(_git_url):
         _git_url = f"{parsed_url.scheme}://{netloc}{parsed_url.path}"
     elif _git_url.startswith('git@'):
         _git_url = _git_url.replace(':', '/', 1).replace('git@', 'https://')
+
     if _git_url.endswith('.git'):
         _git_url = _git_url[:-4]
+
     return _git_url
 
 
